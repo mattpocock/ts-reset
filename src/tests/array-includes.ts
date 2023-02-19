1d83b6ae5bb89f57ee7414ec0500b2203251c734
Matt Pocock <mattpocockvoice@gmail.com>
2023-02-19 18:01:30 +0000

    Added support for array.includes

---
+++ src/tests/array-includes.ts
@@ -0,0 +1,51 @@
+import { doNotExecute } from "./utils";
+
+doNotExecute(async () => {
+  const arr = ["1", "2", "3"] as const;
+
+  // Look ma, no error!
+  arr.includes("4");
+
+  // Look ma, proper errors!
+  arr.includes(
+    // @ts-expect-error
+    2,
+  );
+  arr.includes(
+    // @ts-expect-error
+    true,
+  );
+});
+
+doNotExecute(async () => {
+  const arr = [1, 2, 3] as const;
+
+  arr.includes(4);
+  arr.includes(
+    // @ts-expect-error
+    true,
+  );
+  arr.includes(
+    // @ts-expect-error
+    "str",
+  );
+});
+
+doNotExecute(async () => {
+  const arr = [
+    { a: 1 },
+    {
+      a: 2,
+    },
+    {
+      a: 3,
+    },
+  ] as const;
+
+  arr.includes(
+    // @ts-expect-error
+    4,
+  );
+
+  arr.includes({ a: 1 });
+});

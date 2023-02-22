5903c80a74dc1a67a6324bdb23360c430803d5b1
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-02-22 18:57:44 +0300

    Fixes to isArray, handled when input is `any`

--- src/entrypoints/is-array.d.ts
+++ src/entrypoints/is-array.d.ts
@@ -1,3 +1,12 @@
+type AsArray<T> = T[] extends T ? T[] : any[] 
+/* 
+  The reason I'm using a separate type here called AsArray
+  In cases of "Unarray Test" the type looks confusing like: T & (T[] extends T ? T & T[] : any[])
+  With AsArray it at least looks like this: T & AsArray<T>
+    which is a lot more readable.
+  
+  In other cases it just shows unknown[], any[] or string[] etc as normal.
+*/ 
 interface ArrayConstructor {
-  isArray(arg: any): arg is unknown[];
+  isArray<T>(arg: T | T[]): arg is AsArray<T>
 }
--- src/tests/is-array.ts
+++ src/tests/is-array.ts
@@ -1,10 +1,18 @@
 import { doNotExecute, Equal, Expect } from "./utils";
 
 doNotExecute(() => {
-  const maybeArr = [1, 2, 3] as unknown;
+  const maybeArr = [1, 2, 3] as unknown
 
   if (Array.isArray(maybeArr)) {
     type tests = [Expect<Equal<typeof maybeArr, unknown[]>>];
+  }
+});
+
+doNotExecute(() => {
+  const maybeArr = [1, 2, 3] as any
+
+  if (Array.isArray(maybeArr)) {
+    type tests = [Expect<Equal<typeof maybeArr, any[]>>];
   }
 });
 
@@ -22,4 +30,20 @@ doNotExecute(() => {
   const paths = Array.isArray(path) ? path : [path];
 
   type tests = [Expect<Equal<typeof paths, string[]>>];
+});
+
+// Unarray Test
+doNotExecute(() => {
+  type Unarray<T> = T extends Array<infer U> ? U : T;
+
+  function test<T>(value: T) {
+	
+	  const inner = <X extends Unarray<T>>(v: X[]) => {
+		  // 
+	  }
+	
+	  if (Array.isArray(value)) {
+		  inner(value);
+	  }
+  }
 });

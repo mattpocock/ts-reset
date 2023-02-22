a2a44149d8daef1c350ef3eaa261b0bf70b8ecf0
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2023-02-22 17:04:02 +0100

    Add more tests

--- src/tests/map-constructor.ts
+++ src/tests/map-constructor.ts
@@ -5,5 +5,57 @@ doNotExecute(() => {
 
     const result = map.get('foo');
 
-    type tests = [Expect<Equal<typeof result, unknown>>];
+    type test = [Expect<Equal<typeof result, unknown>>];
+});
+
+doNotExecute(() => {
+    const map = new Map();
+
+    map.set('Jessie', {phone: '213-555-1234', address: '123 N 1st Ave'});
+
+    const result = map.has('Jessie');
+
+    type test = [Expect<Equal<typeof result, boolean>>];
+});
+
+doNotExecute(() => {
+    const map = new Map();
+
+    map.set('Jessie', {phone: '213-555-1234', address: '123 N 1st Ave'});
+
+    const result = map.get('Jessie');
+
+    type test = [Expect<Equal<typeof result, unknown>>];
+});
+
+doNotExecute(() => {
+    const map = new Map();
+
+    map.set('Jessie', {phone: '213-555-1234', address: '123 N 1st Ave'});
+
+    const result = map.delete('Jessie');
+
+    type test = [Expect<Equal<typeof result, boolean>>];
+});
+
+doNotExecute(() => {
+    const map = new Map();
+
+    map.set('Jessie', {phone: '213-555-1234', address: '123 N 1st Ave'});
+    map.set('Hilary', {phone: '617-555-4321', address: '321 S 2nd St'});
+
+    const size = map.size;
+
+    type testSize = [Expect<Equal<typeof size, number>>];
+});
+
+doNotExecute(() => {
+    const map = new Map();
+
+    map.set('Jessie', {phone: '213-555-1234', address: '123 N 1st Ave'});
+    map.set('Hilary', {phone: '617-555-4321', address: '321 S 2nd St'});
+
+    const cleared = map.clear();
+
+    type testClear = [Expect<Equal<typeof cleared, void>>];
 });

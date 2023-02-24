0bccc5a3800eb4951a7118c3fd53ebc096574c00
Benedikt Hopfinger <benedikt.kusemann@klarna.com>
2023-02-24 07:42:49 +0100

    Adds widened has support to ReadonlySet

--- src/tests/set-has.ts
+++ src/tests/set-has.ts
@@ -15,3 +15,19 @@ doNotExecute(() => {
     true,
   );
 });
+
+doNotExecute(() => {
+  const set = new Set([1, 2, 3] as const) as ReadonlySet<1 | 2 | 3>;
+
+  set.has(4);
+
+  set.has(
+    // @ts-expect-error
+    "4",
+  );
+
+  set.has(
+    // @ts-expect-error
+    true,
+  );
+});

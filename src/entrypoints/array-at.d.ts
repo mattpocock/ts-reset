880347121c74ead15bf62cec0ccbe454eb785b15
Flo <fpellet@ensc.fr>
2023-03-06 21:05:09 +0000

    apparently no use to repeat the jsdoc description

--- src/entrypoints/array-at.d.ts
+++ src/entrypoints/array-at.d.ts
@@ -1,10 +1,6 @@
 /// <reference path="utils.d.ts" />
 
 interface ReadonlyArray<T> {
-	/**
-	 * Returns the item located at the specified index.
-	 * @param index The zero-based index of the desired code unit. A negative index will count back from the last item.
-	 */
 	at<I extends number> (index: I): `${I}` extends `-${infer J extends number}`
 		? `${TSReset.Subtract<this["length"], J>}` extends `-${number}`
 			? undefined

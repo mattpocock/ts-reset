1f0e77bb2cd1aea6e414ead844d8219c3293d15c
Flo <fpellet@ensc.fr>
2023-03-07 11:45:11 +0000

    Add test for potentially harmful `undefined` case

--- src/tests/array-at.ts
+++ src/tests/array-at.ts
@@ -29,3 +29,14 @@ doNotExecute(async () => {
     Expect<Equal<typeof d, undefined>>,
   ]
 });
+
+doNotExecute(async () => {
+  const arr = [false, 1, '2'] as const;
+  const index: number = 1
+  const a = arr.at(index)
+  // WARN: with `"strictNullChecks": true,` the correct type here should include `undefined`
+  // but the current implementation does not type this as including `undefined`
+  type tests = [
+    Expect<Equal<typeof a, false | 1 | "2">>,
+  ]
+});

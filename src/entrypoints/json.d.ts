3a278b6cf71fe411cf3f48c528ec3e9c765f82b0
Aadit M Shah <aaditmshah@aadit.codes>
2023-03-23 14:00:11 +0530

    :recycle: Change the order of `JsonValue[]` and `JsonObject`

--- src/entrypoints/json.d.ts
+++ src/entrypoints/json.d.ts
@@ -1,5 +1,5 @@
 declare namespace TSReset {
-  type JsonValue = string | number | boolean | JsonValue[] | JsonObject | null;
+  type JsonValue = string | number | boolean | JsonObject | JsonValue[] | null;
 
   type JsonObject = { [key: string]: JsonValue };
 }

c9b9c3dce7da45813dc5aec2e3488862d6f33639
Aadit M Shah <aaditmshah@aadit.codes>
2023-03-23 16:19:39 +0530

    :white_check_mark: Add positive and negative tests for `reviver`

--- src/tests/json-parse.ts
+++ src/tests/json-parse.ts
@@ -1,4 +1,7 @@
+import { isNumber, isString, isEntries } from "./type-guards";
 import { doNotExecute, Equal, Expect } from "./utils";
+
+const isNumberStringEntries = isEntries(isNumber, isString);
 
 doNotExecute(() => {
   const result = JSON.parse("{}");
@@ -7,8 +10,74 @@ doNotExecute(() => {
 });
 
 doNotExecute(() => {
-  // Make tests fail when someone tries to PR JSON.parse<T>
+  const result = JSON.parse('{"p": 5}', (key, value) =>
+    typeof value === "number" ? value * 2 : value,
+  );
 
-  // @ts-expect-error
-  const result = JSON.parse<string>("{}");
+  type tests = [Expect<Equal<typeof result, unknown>>];
+});
+
+doNotExecute(() => {
+  const result = JSON.parse<TSReset.JsonValue>('{"p": 5}', (key, value) =>
+    typeof value === "number" ? value * 2 : value,
+  );
+
+  type tests = [Expect<Equal<typeof result, TSReset.JsonValue>>];
+});
+
+doNotExecute(() => {
+  const result = JSON.parse('[[1,"one"],[2,"two"],[3,"three"]]', (key, value) =>
+    // @ts-expect-error
+    key === "" ? new Map(value) : value,
+  );
+});
+
+doNotExecute(() => {
+  const result = JSON.parse('[[1,"one"],[2,"two"],[3,"three"]]', (key, value) =>
+    key === "" && isNumberStringEntries(value) ? new Map(value) : value,
+  );
+
+  type tests = [Expect<Equal<typeof result, unknown>>];
+});
+
+doNotExecute(() => {
+  type JsonValueWithMap = TSReset.JsonValue | Map<number, string>;
+
+  const result = JSON.parse<JsonValueWithMap>(
+    '[[1,"one"],[2,"two"],[3,"three"]]',
+    (key, value) =>
+      // @ts-expect-error
+      key === "" && isNumberStringEntries(value) ? new Map(value) : value,
+  );
+});
+
+doNotExecute(() => {
+  type JsonValueWithMap = TSReset.JsonAlgebra<
+    TSReset.JsonValue | Map<number, string>
+  >;
+
+  const result = JSON.parse<JsonValueWithMap>(
+    '[[1,"one"],[2,"two"],[3,"three"]]',
+    (key, value) =>
+      // @ts-expect-error
+      key === "" && isNumberStringEntries(value) ? new Map(value) : value,
+  );
+});
+
+doNotExecute(() => {
+  type JsonValueWithMap =
+    | TSReset.JsonPrimitive
+    | Map<number, string>
+    | JsonObjectWithMap
+    | JsonValueWithMap[];
+
+  type JsonObjectWithMap = { [key: string]: JsonValueWithMap };
+
+  const result = JSON.parse<JsonValueWithMap>(
+    '[[1,"one"],[2,"two"],[3,"three"]]',
+    (key, value) =>
+      key === "" && isNumberStringEntries(value) ? new Map(value) : value,
+  );
+
+  type tests = [Expect<Equal<typeof result, JsonValueWithMap>>];
 });

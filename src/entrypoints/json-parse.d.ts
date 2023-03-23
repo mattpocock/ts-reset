91c257a4b19b2172809a46909d1934057fed346d
Aadit M Shah <aaditmshah@aadit.codes>
2023-03-23 16:16:12 +0530

    :label: Update the type of `JSON.parse` when `reviver` is specified

--- src/entrypoints/json-parse.d.ts
+++ src/entrypoints/json-parse.d.ts
@@ -4,11 +4,21 @@ interface JSON {
   /**
    * Converts a JavaScript Object Notation (JSON) string into an object.
    * @param text A valid JSON string.
+   */
+  parse(text: string): TSReset.JsonValue;
+
+  /**
+   * Converts a JavaScript Object Notation (JSON) string into an object.
+   * @param text A valid JSON string.
    * @param reviver A function that transforms the results. This function is called for each member of the object.
    * If a member contains nested objects, the nested objects are transformed before the parent object is.
    */
-  parse(
+  parse<A = unknown>(
     text: string,
-    reviver?: (this: any, key: string, value: any) => any,
-  ): TSReset.JsonValue;
+    reviver: <K extends string>(
+      this: TSReset.JsonHolder<K, A>,
+      key: K,
+      value: TSReset.JsonAlgebra<A>,
+    ) => A,
+  ): A;
 }

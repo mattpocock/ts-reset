bab16a8217fd8341bbee03a5a52e117ae586b4b6
Aadit M Shah <aaditmshah@aadit.codes>
2023-03-24 23:45:29 +0530

    :label: Add the `JsonComposite` type and move `JsonAlgebra`

--- src/entrypoints/json.d.ts
+++ src/entrypoints/json.d.ts
@@ -1,11 +1,13 @@
 declare namespace TSReset {
   type JsonPrimitive = string | number | boolean | null;
 
+  type JsonComposite<A> = Record<string, A> | A[];
+
+  type JsonAlgebra<A> = JsonPrimitive | JsonComposite<A>;
+
   type JsonValue = JsonPrimitive | JsonObject | JsonValue[];
 
   type JsonObject = { [key: string]: JsonValue };
-
-  type JsonAlgebra<A> = JsonPrimitive | Record<string, A> | A[];
 
   type JsonHolder<K extends string, A> = Record<K, JsonAlgebra<A>>;
 }

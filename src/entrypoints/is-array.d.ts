d2918bb78b48874bfa27ec9b692c372f51b36945
Mickemouse0 <31856324+Mickemouse0@users.noreply.github.com>
2023-06-18 01:05:23 +0200

    Update is-array.d.ts

--- src/entrypoints/is-array.d.ts
+++ src/entrypoints/is-array.d.ts
@@ -1,3 +1,3 @@
 interface ArrayConstructor {
-  isArray<T>(arg: 0 extends 1 & T ? never : T): arg is 0 extends 1 & T ? never : T extends Array<unknown> | ReadonlyArray<unknown> ? T : T[] extends T ? T[] : never;
+  isArray<T>(arg: 0 extends 1 & T ? never : T): arg is 0 extends 1 & T ? never : T extends unknown[] | readonly unknown[] ? T : T[] extends T ? T[] : never;
 }

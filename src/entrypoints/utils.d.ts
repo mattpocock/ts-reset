db19046f4e69ed55a52e5ddb2a47070ae6b7d3c7
Mickemouse0 <31856324+Mickemouse0@users.noreply.github.com>
2023-06-20 07:12:37 +0200

    Fix indentation

--- src/entrypoints/utils.d.ts
+++ src/entrypoints/utils.d.ts
@@ -15,6 +15,6 @@ declare namespace TSReset {
     ? symbol
     : T;
 
-    // https://stackoverflow.com/questions/49927523/disallow-call-with-any/49928360#49928360
+  // https://stackoverflow.com/questions/49927523/disallow-call-with-any/49928360#49928360
   type IsAny<T> = 0 extends 1 & T ? true : false;
 }

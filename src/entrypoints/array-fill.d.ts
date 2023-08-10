bf891890831bc7819eac34cc519dfad205562da4
genzzo <alikanso.504@gmail.com>
2023-08-10 18:59:09 +0300

    fix: made fill a never type on readonly array and refactored the typing on normal array

--- src/entrypoints/array-fill.d.ts
+++ src/entrypoints/array-fill.d.ts
@@ -1,17 +1,15 @@
 /// <reference path="utils.d.ts" />
 
+// TODO: See how to remove the fill method from the interface completely
 interface ReadonlyArray<T> {
-  fill<U>(
-    value: U | (TSReset.WidenLiteral<U> & {}),
-    start?: number | undefined,
-    end?: number | undefined,
-  ): U[];
+  fill: never;
 }
 
+// TODO: See how to override the fill method so that an Array<T>.fill<U> makes the Array<T> an Array<U>
 interface Array<T> {
   fill<U>(
     value: U | (TSReset.WidenLiteral<U> & {}),
-    start?: number | undefined,
-    end?: number | undefined,
-  ): U[];
+    start?: number,
+    end?: number,
+  ): (U | (TSReset.WidenLiteral<U> & {}))[];
 }

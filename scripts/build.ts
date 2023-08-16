faf46f1bfd3348a6c7e5a734e51088cee1a6e751
MDReal <veysaliyev00@gmail.com>
2023-08-16 14:08:03 +0400

    update package.json exports path

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,8 +1,25 @@
 import * as fs from "fs/promises";
 import * as path from "path";
+// import pkgJson from "../package.json";
+
+// interface Export {
+//   types: string;
+//   import: string;
+//   default: string;
+// }
+// type Exports = Record<string, string | Export>;
 
 const entrypointDir = path.join(__dirname, "../", "src", "entrypoints");
 const distDir = path.join(__dirname, "../", "dist");
+
+// const exports: Exports = {
+//   "./package.json": "./package.json",
+//   ".": {
+//     types: "./dist/recommended.d.ts",
+//     import: "./dist/recommended.mjs",
+//     default: "./dist/recommended.js",
+//   },
+// };
 
 const run = async () => {
   try {
@@ -13,8 +30,15 @@ const run = async () => {
 
   for (const entrypoint of entrypoints) {
     const entrypointBase = entrypoint.replace(".d.ts", "");
+    // const unixBasedEntrypointBase = entrypointBase.replace(/[\\/]/g, "/");
 
     await Promise.all([
+
+      // exports[`./${path.dirname(unixBasedEntrypointBase)}`] = {
+      //   types: `./dist/index.d.ts`,
+      //   import: `./dist/index.mjs`,
+      //   default: `./dist/index.js`,
+      // };
       fs.writeFile(path.join(distDir, `${entrypointBase}.js`), ""),
       fs.writeFile(path.join(distDir, `${entrypointBase}.mjs`), ""),
       fs.copyFile(
@@ -22,7 +46,19 @@ const run = async () => {
         path.join(distDir, `${entrypointBase}.d.ts`),
       ),
     ]);
+
+    // exports[`./${unixBasedEntrypointBase}`] = {
+    //   types: `./dist/${unixBasedEntrypointBase}.d.ts`,
+    //   import: `./dist/${unixBasedEntrypointBase}.mjs`,
+    //   default: `./dist/${unixBasedEntrypointBase}.js`,
+    // };
   }
+
+  // pkgJson.exports = exports as typeof pkgJson.exports;
+  // await fs.writeFile(
+  //   path.join(__dirname, "../", "package.json"),
+  //   JSON.stringify(pkgJson, null, 2),
+  // );
 };
 
 run().catch((e) => {

8c520d0821c55167cadfdacad5d2c72f3395c464
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-08-21 10:11:05 +0000

    prettier format

--- src/entrypoints/array-at.d.ts
+++ src/entrypoints/array-at.d.ts
@@ -1,13 +1,13 @@
 /// <reference path="utils.d.ts" />
 
 interface ReadonlyArray<T> {
-	at<const I extends number>(
-	  index: I
-	): TSReset.Equal<I, number> extends true
-	  ? T | undefined
-	  : `${I}` extends `-${infer J extends number}`
-	  ? `${TSReset.Subtract<this["length"], J>}` extends `-${number}`
-		? undefined
-		: this[TSReset.Subtract<this["length"], J>]
-	  : this[I];
+  at<const I extends number>(
+    index: I,
+  ): TSReset.Equal<I, number> extends true
+    ? T | undefined
+    : `${I}` extends `-${infer J extends number}`
+    ? `${TSReset.Subtract<this["length"], J>}` extends `-${number}`
+      ? undefined
+      : this[TSReset.Subtract<this["length"], J>]
+    : this[I];
 }
--- src/tests/array-at.ts
+++ src/tests/array-at.ts
@@ -1,66 +1,60 @@
 import { doNotExecute, Equal, Expect } from "./utils";
 
 doNotExecute(async () => {
-  const arr = [false, 1, '2'] as const;
+  const arr = [false, 1, "2"] as const;
 
-  const a = arr.at(0)
-  const b = arr.at(1)
-  const c = arr.at(2)
-  const d = arr.at(3)
+  const a = arr.at(0);
+  const b = arr.at(1);
+  const c = arr.at(2);
+  const d = arr.at(3);
   type tests = [
     Expect<Equal<typeof a, false>>,
     Expect<Equal<typeof b, 1>>,
-    Expect<Equal<typeof c, '2'>>,
+    Expect<Equal<typeof c, "2">>,
     Expect<Equal<typeof d, undefined>>,
-  ]
+  ];
 });
 
 doNotExecute(async () => {
-  const arr = [false, 1, '2'] as const;
+  const arr = [false, 1, "2"] as const;
 
-  const a = arr.at(-1)
-  const b = arr.at(-2)
-  const c = arr.at(-3)
-  const d = arr.at(-4)
+  const a = arr.at(-1);
+  const b = arr.at(-2);
+  const c = arr.at(-3);
+  const d = arr.at(-4);
   type tests = [
-    Expect<Equal<typeof a, '2'>>,
+    Expect<Equal<typeof a, "2">>,
     Expect<Equal<typeof b, 1>>,
     Expect<Equal<typeof c, false>>,
     Expect<Equal<typeof d, undefined>>,
-  ]
+  ];
 });
 
 doNotExecute(async () => {
-  const arr = [false, 1, '2'] as const;
+  const arr = [false, 1, "2"] as const;
 
   function index() {
-    return Math.random() > 0.5 ? 0 : 1
+    return Math.random() > 0.5 ? 0 : 1;
   }
 
-  const a = arr.at(index())
-  type tests = [
-    Expect<Equal<typeof a, false | 1>>,
-  ]
+  const a = arr.at(index());
+  type tests = [Expect<Equal<typeof a, false | 1>>];
 });
 
 doNotExecute(async () => {
-  const arr = [false, true, 1, '2'] as const;
+  const arr = [false, true, 1, "2"] as const;
 
   function index() {
-    return Math.random() > 0.5 ? -1 : -2
+    return Math.random() > 0.5 ? -1 : -2;
   }
 
-  const a = arr.at(index())
-  type tests = [
-    Expect<Equal<typeof a, '2' | 1>>,
-  ]
+  const a = arr.at(index());
+  type tests = [Expect<Equal<typeof a, "2" | 1>>];
 });
 
 doNotExecute(async () => {
-  const arr = [false, 1, '2'] as const;
-  const index = 1 as number
-  const a = arr.at(index)
-  type tests = [
-    Expect<Equal<typeof a, false | 1 | "2" | undefined>>,
-  ]
+  const arr = [false, 1, "2"] as const;
+  const index = 1 as number;
+  const a = arr.at(index);
+  type tests = [Expect<Equal<typeof a, false | 1 | "2" | undefined>>];
 });

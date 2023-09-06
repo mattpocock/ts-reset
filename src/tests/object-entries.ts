c559bfdff01c76746592d78b8088bc61b22bd1c7
PCOffline <bekerman122@gmail.com>
2023-09-06 12:11:54 +0300

    test(object-entries): add check for a string index in arrays

--- src/tests/object-entries.ts
+++ src/tests/object-entries.ts
@@ -11,6 +11,17 @@ doNotExecute(async () => {
     // Complex values
     Expect<Equal<typeof result2, (['a', true] | ['b', 'string'] | ['c', { readonly field: 1 }])[]>>,
     // Array
-    Expect<Equal<typeof result3, [number, 1 | 2 | 3][]>>,
+    Expect<Equal<typeof result3, [`${number}`, 1 | 2 | 3][]>>,
   ];
+});
+
+doNotExecute(async () => {
+  const array = [1, 2, 3];
+  const readonlyArray = [1, 2, 3] as const;
+  const result1 = Object.entries(array);
+  const result2 = Object.entries(readonlyArray);
+
+  // Make sure arrays' index is a string in the result
+  result1[0][0] satisfies `${number}`;
+  result2[0][0] satisfies `${number}`;
 });

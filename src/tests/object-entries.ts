0bb852d066bffd71b96953d09e1501f45fb622f9
PCOffline <bekerman122@gmail.com>
2023-09-06 11:45:31 +0300

    test: add tests for Object.entries

---
+++ src/tests/object-entries.ts
@@ -0,0 +1,16 @@
+import { doNotExecute, Equal, Expect } from './utils';
+
+doNotExecute(async () => {
+  const result1 = Object.entries({ a: 1, b: 2, c: 3 });
+  const result2 = Object.entries({ a: true, b: 'string', c: { field: 1 } });
+  const result3 = Object.entries([1, 2, 3]);
+
+  type tests = [
+    // Basic functionality test
+    Expect<Equal<typeof result1, (['a', 1] | ["b", 2] | ['c', 3])[]>>,
+    // Complex values
+    Expect<Equal<typeof result2, (['a', true] | ['b', 'string'] | ['c', { readonly field: 1 }])[]>>,
+    // Array
+    Expect<Equal<typeof result3, [number, 1 | 2 | 3][]>>,
+  ];
+});

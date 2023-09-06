d96d21bc37c7d4eb36ac001948dbc2abbb7863fe
PCOffline <bekerman122@gmail.com>
2023-09-06 12:12:11 +0300

    fix(object-entries); make arrays return string index

--- src/entrypoints/object-entries.d.ts
+++ src/entrypoints/object-entries.d.ts
@@ -1,3 +1,3 @@
 interface ObjectConstructor {
-  entries<T>(o: T): T extends ArrayLike<any> ? { [K in Extract<keyof T, number>]: [K, T[K]] }[number][] : { [K in keyof T]: [K, T[K]] }[keyof T][];
+  entries<T>(o: T): T extends ArrayLike<any> ? { [K in Extract<keyof T, number>]: [`${K}`, T[K]] }[number][] : { [K in keyof T]: [K, T[K]] }[keyof T][];
 }

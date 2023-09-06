a8a661f3545d5743c5f9c3b5a2f7f18625bda01a
PCOffline <bekerman122@gmail.com>
2023-09-06 11:57:13 +0300

    fix(object-entries): downgrade to TypeScript@4

--- src/tests/object-entries.ts
+++ src/tests/object-entries.ts
@@ -1,9 +1,9 @@
 import { doNotExecute, Equal, Expect } from './utils';
 
 doNotExecute(async () => {
-  const result1 = Object.entries({ a: 1, b: 2, c: 3 });
-  const result2 = Object.entries({ a: true, b: 'string', c: { field: 1 } });
-  const result3 = Object.entries([1, 2, 3]);
+  const result1 = Object.entries({ a: 1, b: 2, c: 3 } as const);
+  const result2 = Object.entries({ a: true, b: 'string', c: { field: 1 } } as const);
+  const result3 = Object.entries([1, 2, 3] as const);
 
   type tests = [
     // Basic functionality test

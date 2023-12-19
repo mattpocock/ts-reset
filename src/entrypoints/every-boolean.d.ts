f1538ca3fbe53a9700a9483fc4a3acd4c46f60d4
Fea <feathecutie@disroot.org>
2023-12-19 14:23:13 +0100

    Fixed Array.every improvements breaking existing code

--- src/entrypoints/every-boolean.d.ts
+++ src/entrypoints/every-boolean.d.ts
@@ -1,23 +1,23 @@
 /// <reference path="utils.d.ts" />
 
 interface Array<T> {
-  every(
+  every<S extends T>(
     predicate: BooleanConstructor,
     thisArg?: any,
-  ): this is this extends T[]
-    ? TSReset.NonFalsy<T>[]
-    : this[number] extends TSReset.NonFalsy<T>
+  ): this is this extends S[]
+    ? TSReset.NonFalsy<S>[]
+    : this[number] extends TSReset.NonFalsy<S>
     ? this
     : never;
 }
 
 interface ReadonlyArray<T> {
-  every(
+  every<S extends T>(
     predicate: BooleanConstructor,
     thisArg?: any,
-  ): this is this extends T[]
-    ? TSReset.NonFalsy<T>[]
-    : this[number] extends TSReset.NonFalsy<T>
+  ): this is this extends S[]
+    ? TSReset.NonFalsy<S>[]
+    : this[number] extends TSReset.NonFalsy<S>
     ? this
     : never;
 }

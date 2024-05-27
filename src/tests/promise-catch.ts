66a7182a98d5c4b361d7eb6674f7e99315d061d6
Matt Pocock <mattpocockvoice@gmail.com>
2024-05-27 17:01:54 +0100

    Formatted

--- src/tests/promise-catch.ts
+++ src/tests/promise-catch.ts
@@ -1,13 +1,14 @@
 import { doNotExecute, Equal, Expect } from "./utils";
 
 doNotExecute(async () => {
-  Promise.reject('string instead of Error').catch((err) => {
-    // @ts-expect-error
-    err.cause;
+  Promise.reject("string instead of Error").catch((err) => {
+    type test = Expect<Equal<unknown, typeof err>>;
   });
 
-  Promise.reject('string instead of Error').then(() => {}, (err) => {
-    // @ts-expect-error
-    err.cause;
-  });
+  Promise.reject("string instead of Error").then(
+    () => {},
+    (err) => {
+      type test = Expect<Equal<unknown, typeof err>>;
+    },
+  );
 });
